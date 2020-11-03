1bce19183d7b2e217a25bbb8d77c98a47704099b
Andrew-CC-Martin <amartin262@gmail.com>
2020-11-03 17:49:04 +1100

    added connecting lines

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # particle-constellations
-Project to create a funky little animation with JavaScript
+Project to create a funky little animation with JavaScript.
+
+Uses [PixiJS library](https://www.pixijs.com/), which is using webgl under the hood. If the browser doesn't have webgl supported, it'll use a regular canvas instead. The only real difference is that you'll lose graphics card support.
--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-// Get a random number that's between x and y
-// (where y is positive and > x)
-function getRandomFromXtoY(x, y) {
-  const r = Math.random() * y
-  if (r > x) {
-    return r
-  } else {
-    getRandomFromXtoY(x, y)
-  }
-}
+// constants
+const STARS_COUNT = 50
+const THICKNESS_FACTOR = 500
+const THICKNESS_MAX = 1
+const OPACITY_FACTOR = 100
 
 // Create our PIXI webgl canvas and add it to DOM
 let app = new PIXI.Application({
@@ -19,26 +14,21 @@ document.body.appendChild(app.view)
 // Create the main stage for your display objects
 var stage = new PIXI.Container()
 
-const starsCount = 100
-const starSize = 2
-
 // Create the initial states for each star
 const stars = []
-for (let i = 0; i < starsCount; i++) {
+for (let i = 0; i < STARS_COUNT; i++) {
   // get star position
   const x = Math.random() * window.innerWidth
   const y = Math.random() * window.innerHeight
   const vx = Math.random() * (Math.round(Math.random()) ? 1 : -1)
   const vy = Math.random() * (Math.round(Math.random()) ? 1 : -1)
   const colour = `0x${Math.floor(Math.random()*16777215).toString(16)}`
-  const size = getRandomFromXtoY(20, 70)
   stars.push({
     x,
     y,
     vx,
     vy,
     colour,
-    size
   })
 }
 
@@ -51,11 +41,11 @@ stage.addChild(graphics)
 // Start animating
 animate()
 function animate() {
-  // Wipe the canvas clear
+  // Wipe the canvas clear before each animation frame
   graphics.clear()
 
   // Update the positions for each star
-  for (let i = 0; i < starsCount; i++) {
+  for (let i = 0; i < STARS_COUNT; i++) {
     // Get new position (current position + velocity)
     const newX = stars[i].x + stars[i].vx
     const newY = stars[i].y + stars[i].vy
@@ -65,22 +55,37 @@ function animate() {
     if ((newX < 0) || (newX > window.innerWidth)) {
       stars[i].vx = -stars[i].vx
     }
-    if ((newY < 0) || (newY > window.innerWidth)) {
+    if ((newY < 0) || (newY > window.innerHeight)) {
       stars[i].vy = -stars[i].vy
     }
 
     stars[i].y = stars[i].y + stars[i].vy
     stars[i].x = stars[i].x + stars[i].vx
-
-    // Set the fill color
-    graphics.beginFill(stars[i].colour)
-
-    // Draw the star
-    graphics.drawCircle(stars[i].x, stars[i].y, stars[i].size)
-
-    // Applies fill to lines and shapes since the last call to beginFill.
-    graphics.endFill()
   }
+
+  // Draw the lines between the stars
+  for (let i = 0; i < STARS_COUNT; i++) {
+    for (let j = 0; j < STARS_COUNT; j++) {
+      // No need for star to check against self
+      if (i === j) {
+        break
+      }
+      const distanceX = stars[i].x - stars[j].x
+      const distanceY = stars[i].y - stars[j].y
+      const distance = ((distanceY ** 2) + (distanceX ** 2)) ** 0.5
+
+        // draw line from (stars[i].x, stars[i].y) to (stars[j].x, stars[j].y)
+        const opacity = OPACITY_FACTOR / distance
+        const thicknessCalculated = (THICKNESS_FACTOR / distance)
+        const thickness = thicknessCalculated > THICKNESS_MAX ? THICKNESS_MAX : thicknessCalculated
+
+        // Draw the line (endPoint should be relative to myGraph's position)
+        graphics.lineStyle(thickness, stars[i].colour, opacity)
+                .moveTo(stars[i].x, stars[i].y)
+                .lineTo(stars[j].x, stars[j].y)
+    }
+  }
+
   //Render the stage
   app.renderer.render(stage)
   requestAnimationFrame(animate)
